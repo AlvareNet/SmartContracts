2bcec5d17012def5d4999cd14772416c0039950f
leonardmgh <leonard@hansen-net.eu>
2021-08-16 12:34:12 +0200

    Fixed floating compiler version

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
 
 //Imports from openzeppelin library
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
